9ddc3a51ba897807256665f8248e38e67c93490a
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2022-11-29 20:28:26 +0100

    2

--- nacafe-lora/src/index.js
+++ nacafe-lora/src/index.js
@@ -1,3 +1,15 @@
 import './style.css';
 
 console.log('funguju!');
+
+import { Banner } from './components/Banner/index.js';
+import { Contact } from './components/Contact/index.js';
+import { Gallery } from './components/Gallery/index.js';
+import { Footer } from './components/Footer/index.js';
+import { Header } from './components/Header/index.js';
+import { Menu } from './components/Menu/index.js';
+
+const pageelement = document.createElement('div');
+pageelement.classList.add('page');
+
+document.querySelector('#app').append(pageElement);
